4b6f6b3aa493be47675f291cf21cb352cc8742c3
ming <fox20431@gmail.com>
2024-10-03 10:35:18 +0800

    init

---
+++ config_list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define configs to backup/restore
+configs=(
+    "$HOME/.config/sway"
+    "$HOME/.config/waybar"
+    "$HOME/.config/swaylock"
+    "$HOME/.config/kitty"
+    "$HOME/.config/fontconfig"
+)
